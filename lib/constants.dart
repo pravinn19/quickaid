f775ff34164f72affed06407453ac55af96dbc70
Pravin V <pravinmav1903@gmail.com>
2021-11-09 22:09:10 +0530

    added new image

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 const String appName = "QuickAid";
 const String slogan = "Mitigate the hustle";
 const String bgimage = "assets/pexels-pixabay-280221.jpg";
+const String handsimage = "assets/hands.jpg";
 const String loginin = "LOGIN";
 const String forgettext = "Forget Password?";
